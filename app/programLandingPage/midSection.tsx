6d14b0efa9b711b08f29681190cf8fb187ae45d1
Yisaterese <teresejosephyisa@gmail.com>
2024-10-23 11:09:47 +0100

    added code on midsection component, modules and sessions

--- app/programLandingPage/midSection.tsx
+++ app/programLandingPage/midSection.tsx
@@ -1,16 +1,20 @@
+import instructorImage from '../../public/midSection/instructorImage.png';
+import liveVideoImage from '../../public/midSection/liveVideoImage.png';
+import {Icon} from '@iconify/react';
 import React from 'react';
-import ButtonProps from '../../component/button';
+import Image from 'next/image';
+
 export default function MidSection(){
     return(
         <div className={'flex'}>
-            <div className={'w-[70%]'}>
+            <div className={'w-[70%] border-r-4'}>
                 <div className={'flex gap-3 text-black ml-[7%]'}>
                     <p>About</p>
                     <p>Program goals</p>
                     <p>Program benefits</p>
                     <p>Courses</p>
                 </div>
-                <div className={'ml-[7%] m-4'}>
+                <div className={'ml-[7%] m-4 mr-[8%] '}>
                     <h1 className={'text-black text-lg font-bold'}>About Program</h1>
                     <div className={'text-black text-xs mt-3 '}>
                         This course examines important issues in corporate finance from the perspectives of financial
@@ -21,17 +25,17 @@ export default function MidSection(){
                     </div>
                     <h1 className={'text-black text-lg font-bold m-6 ml-0'}>Program goals</h1>
                     <div className={'text-black text-xs mt-3 '}>
-                        <h2>What you are expected to know after this course</h2>
-                        <ul>Understand forms of market imperfections and their implications for financial managers</ul>
-                        <ul>Generate a valuation range for a project or a company</ul>
-                        <ul>Apply option theories to solve corporate finance problems</ul>
-                        <ul>Use Excel to conduct a simple DCF analysis, regression analysis and sensitivity analysis
-                        </ul>
-                        <ul>Understand various forms of market imperfections and their implications for financial</ul>
-                        <ul>Generate a valuation range for a project or a company</ul>
-                        <ul>Apply option theories to solve corporate finance problems</ul>
-                        <ul>Use Excel to conduct a simple DCF analysis, regression analysis and sensitivity analysis
-                        </ul>
+                        <h2 className={'mb-3 text-m font-semibold'}>What you are expected to know after this course</h2>
+                        <li>Understand forms of market imperfections and their implications for financial managers</li>
+                        <li>Generate a valuation range for a project or a company</li>
+                        <li>Apply option theories to solve corporate finance problems</li>
+                        <li>Use Excel to conduct a simple DCF analysis, regression analysis and sensitivity analysis
+                        </li>
+                        <li>Understand various forms of market imperfections and their implications for financial</li>
+                        <li>Generate a valuation range for a project or a company</li>
+                        <li>Apply option theories to solve corporate finance problems</li>
+                        <li>Use Excel to conduct a simple DCF analysis, regression analysis and sensitivity analysis
+                        </li>
                     </div>
                     <h1 className={'text-black text-lg font-bold m-6 ml-0'}>Courses</h1>
                     <h2 className={'text-black text-xs mt-3 '}>There are 7 courses in this program</h2>
@@ -41,13 +45,169 @@ export default function MidSection(){
                         className={'text-black text-thin  m-6 mb-2 ml-0'}> Credit unit</span>
                     </div>
                     <h2 className={'text-black text-lg font-bold border-b-4'}>Business Ethics</h2>
-                    <h1>Description</h1>
+                    <h1 className={'text-black text-lg font-bold m-6 ml-0'}>Description</h1>
+                    <h2 className={'text-black text-xs mt-3 '}>
+                        This course examines important issues in corporate finance from the perspectives of financial managers
+                        who make important investment decisions and financing decisions. This course incorporates an element
+                        of financial modelling in teaching and assessments.
+                    </h2>
+                    <h1 className={'text-black text-lg font-bold m-6 ml-0'}>Objectives</h1>
+                    <h2 className={'text-black text-xs mt-3 '}>
+                        This course examines important issues in corporate finance from the perspectives of financial managers
+                        who make important investment decisions and financing decisions. This course incorporates an element
+                        of financial modelling in teaching and assessments.
+                    </h2>
+                    <h1 className={'text-black text-lg font-bold m-6 mt-[50px] ml-0 mb-3'}>Instructor</h1>
+                    <div className={'flex gap-2'}>
+                        <Image src={instructorImage} alt={'instructorImage'}/>
+                        <div className={'flex-col     '}>
+                            <h1 className={'text-black text-lg'}>Olamide Adebisi,PH.D.</h1>
+                            <h2 className={'text-black text-xs'}>Henley Business School • Head Professor </h2>
+                        </div>
+                    </div>
+                    <h2 className={'text-black text-xs mt-3 '}>Olamide taught Donald Trump in Kindergarten. She has 25 years experience teaching presidents design related courses. She has a PhD in Education management
+                        and is a member of faculty at the Henley Business School.
+                    </h2>
+                    <div>
+                        <h1 className={'text-black text-lg font-bold m-6 ml-0 mt-[50px]'}>Instructor’s Introductory
+                            Message</h1>
+                        <h2 className={'text-black text-xs mt-3 '}>Here’s a message from the instructor. Click play to
+                            watch it.</h2>
+                    </div>
+                    <h1 className={'text-black text-lg font-bold m-6 ml-0 mt-[50px]'}>Instructor’s Introductory
+                        Message</h1>
+                    <h2 className={'text-black text-xs mt-3 mb-2'}>Here’s a message from the instructor. Click play to watch it.</h2>
+                    <Image src={liveVideoImage} alt={'liveVideoImage'} className={''}/>
+                    <div className={'mt-[50px]'}>
+                        <h1 className={'text-black text-lg font-bold m-6 ml-0 bg-[#FBFBFB]'}>Modules</h1>
+                        <div className={'mb-5 ml-5 bg-[#FBFBFB] mr-[350px] flex justify-between'}>
+                            <h1 className={'text-blue-500 '}>Module 1 </h1>
+                            <Icon icon="stash:chevron-down" className={' text-black w-8 h-8 '}/>
+                        </div>
+                        <div className={'bg-[#FBFBFB]  '}>
+                            <h1 className={'mb-3 text-sm font-m text-black ml-5 pt-2'}>At the end of the module, the learner
+                                should:
+                            </h1>
+                            <div className={'text-black text-xs m-6 ml-5 pb-1'}>
+                                <li className={'m-6 ml-0'}>Understand various forms of market imperfections and their
+                                    implications for financial
+                                    managers
+                                </li>
+                                <li className={'m-6 ml-0'}>Generate a valuation range for a project or a company
+                                </li>
+                                <li className={'m-6 ml-0'}>Apply option theories to solve corporate finance problems
+                                </li>
+                                <li className={'m-6 ml-0'}> Use Excel to conduct a simple DCF analysis, regression
+                                    analysis and sensitivity
+                                    analysis
+                                </li>
+                            </div>
+                        </div>
+
+                        <div>
+                            <p className={'text-black text-m m-10 mb-4 ml-5'}>Sessions</p>
+                            <div className={'rounded bg-[#FBFBFB]'}>
+                                <div className={'flex justify-between ml-5'}>
+                                    <div className={'flex-col w-[40%]'}>
+                                        <h2 className={'text-black text-xs my-2 '}>SESSION 1</h2>
+                                        <h1 className={'text-black text-lg '}>How International Business Bloom</h1>
+                                    </div>
+                                    <div className={'w-[25%] flex  gap-2'}>
+                                        <h1 className={'text-black align-center justify-center'}>0% Complete</h1>
+                                        <h1 className={' text-black font-bold w-3 align-center justify-center'}>-</h1>
+                                    </div>
+                                </div>
+                                <div className={'ml-5 text-black m-8 mb-2'}>
+                                    <h1 className={'my-2 '}>Description</h1>
+                                    <h2 className={'text-xs'}>This course examines important issues in corporate finance
+                                        from the perspectives of financial
+                                        managers who make important investment decisions and financing decisions.
+                                        This </h2>
+                                </div>
+                                <p className={'text-blue-500 mx-5 text-xs '}>See more</p>
+                                <h1 className={'border-b-2  ml-5 my-6'}></h1>
+                                <p className={'text-black ml-5 '}>Learning Activities</p>
+                                <div className={'ml-7 text-black my-5'}>
+                                    <div className={'flex gap-2'}>
+                                        <h1 className={'font-semibold-[500]'}>Video: </h1>
+                                        <p className={'text-xs m-1 ml-0'}>Introduction to Module</p>
+                                    </div>
+                                    <div className={'text-black flex gap-2 text-xs my-1'}>
+                                        <Icon icon="bitcoin-icons:clock-outline" className={'w-4 h-4'}/>
+                                        <p>3 mins</p>
+                                    </div>
+                                </div>
+                                <h1 className={'border-b-2  ml-5 '}></h1>
+                                <div className={'ml-7 text-black my-5'}>
+                                    <div className={'flex gap-2'}>
+                                        <h1 className={'font-semibold-[500]'}>Reading: </h1>
+                                        <p className={'text-xs m-1 ml-0'}>Learning Content 1</p>
+                                    </div>
+                                    <div className={'text-black flex gap-2 text-xs my-1'}>
+                                        <Icon icon="bitcoin-icons:clock-outline" className={'w-4 h-4'}/>
+                                        <p>3 mins</p>
+                                    </div>
+                                </div>
+                                <h1 className={'border-b-2  ml-5 '}></h1>
+                                <div className={'ml-7 text-blue-500 my-5'}>
+                                    <div className={'flex gap-2'}>
+                                        <h1 className={'font-semibold-[500]'}>Video: </h1>
+                                        <p className={'text-xs m-1 ml-0'}>Learning Content 2</p>
+                                    </div>
+                                    <div className={'flex gap-2 text-xs my-1'}>
+                                        <Icon icon="bitcoin-icons:clock-outline" className={'w-4 h-4'}/>
+                                        <p>3 mins</p>
+                                    </div>
+                                </div>
+                                <h1 className={'border-b-2  ml-5 '}></h1>
+                                <div className={'ml-7 text-black my-5'}>
+                                    <div className={'flex gap-2'}>
+                                        <h1 className={'font-semibold-[500]'}>Audio: </h1>
+                                        <p className={'text-xs m-1 ml-0'}>Learning Content 2</p>
+                                    </div>
+                                    <div className={'text-black flex gap-2 text-xs my-1'}>
+                                        <Icon icon="bitcoin-icons:clock-outline" className={'w-4 h-4'}/>
+                                        <p>3 mins</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={''}>
+                            <div className={'bg-[#FBFBFB] py-3'}>
+                                <div className={'text-black ml-5    '}>
+                                    <div className={'flex justify-between'}>
+                                        <div>
+                                            <p className={'text-xs'}>SESSION 2</p>
+                                            <p className={'font-medium'}>Introduction to Accounting</p>
+                                        </div>
+                                        <p className={'border-[1.5px] rounded w-6 h-7 m-3 pt-0.5'}>
+                                            <Icon icon="clarity:plus-line" className={'m-0.5'}/>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={'bg-[#FBFBFB] py-3 my-3'}>
+                                <div className={'text-black ml-5    '}>
+                                    <div className={'flex justify-between'}>
+                                        <div>
+                                            <p className={'text-xs'}>SESSION 3</p>
+                                            <p className={'font-medium'}>Prezi Presentation Tool</p>
+                                        </div>
+                                        <p className={'border-[1.5px] rounded w-6 h-7 m-3 pt-0.5'}>
+                                            <Icon icon="clarity:plus-line" className={'m-0.5'}/>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            {/*netxt div*/}
+                        </div>
+                    </div>
                 </div>
             </div>
 
             <div className={'w-[30%]'}>
-                <div className={'rounded mt-4 ml-5 mr-6'} >
-                    <div className={''} style={{color:'#b3e2ef'}} >
+                <div className={'rounded mt-4 ml-5 mr-6'}>
+                    <div className={''} style={{color: '#b3e2ef'}}>
 
                         <div>
                             {/*<ButtonProps word={'Apply Now'} className={'bg-blue-700 rounded '}/>*/}
