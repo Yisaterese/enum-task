d5d1da88a37fcec27a24d6a43cc317ea2a3a8555
Yisaterese <teresejosephyisa@gmail.com>
2024-10-24 05:12:06 +0100

    added a component confirmedPayment

---
+++ app/confirmPayment/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/component/navbar";
+import PaymentConfirmedPage from "@/app/confirmPayment/paymentConfirmed";
+
+export default function Page(){
+  return(
+      <div>
+          <Navbar/>
+          <PaymentConfirmedPage/>
+      </div>
+  );
+}
