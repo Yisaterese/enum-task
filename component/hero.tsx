4ad2ae58d29081fad913db5c0ff4a40d92fbf4e5
Yisaterese <teresejosephyisa@gmail.com>
2024-10-29 04:35:03 +0100

    restyled the component and the partners

--- component/hero.tsx
+++ component/hero.tsx
@@ -5,12 +5,13 @@ import semicolonLogo from "../public/landingPagePaid/hero/semicolonLogo.png";
 import heroImage from "../public/landingPagePaid/hero/heroImage.png";
 import { Icon } from '@iconify/react';
 import Button from "../component/button";
+import Link from "next/link";
 
 const Hero = () => {
     return (
-        <div className="flex  lg:flex-row" style={{ height: 'auto', minHeight: '380px' }}>
-            <div className="w-full lg:w-[60%]" style={{ backgroundColor: '#2938B6' }}>
-                <div className={` flex xsm:text-[8px] flex-wrap ml-[5%] lg:ml-[50px] text-white mt-4 lg:mt-6 px-[8%] lg:px-0`}>
+        <div className={` flex lg:flex-row`} style={{  }}>
+            <div className=' ' style={{ backgroundColor: '#2938B6' ,width:'60%'}}>
+                <div className="flex xsm:text-[8px]  mt-8 flex-wrap ml-[5%] lg:ml-[50px] text-white mt-4 lg:mt-6 px-[8%] lg:px-0">
                     <div className="flex items-center mr-2">
                         <p>All Programs</p>
                     </div>
@@ -30,14 +31,16 @@ const Hero = () => {
 
                 <div className="flex items-center p-2 mt-4 ml-[5%] lg:ml-[7%]">
                     <Image src={leftArrow} alt="leftArrow" />
-                    <Button icon={leftArrow} word="back" className="ml-2" />
+                    <Link href={''}>
+                        <Button icon={leftArrow} word="back" className="ml-2" />
+                    </Link>
                 </div>
 
                 <h1 className="text-[24px] lg:text-[30px] font-semibold ml-[5%] lg:ml-[7%] mt-4 lg:mt-6">
                     Business Leadership
                 </h1>
 
-                <div className="flex items-center gap-4 ml-[5%] lg:ml-[7%] mt-4">
+                <div className="flex items-center gap-4 ml-[5%] lg:ml-[7%] mt-8">
                     <Image src={semicolonLogo} alt="semicolonImage" />
                     <div className="mt-1">
                         <p className="text-[10px] lg:text-[12px]">OFFERED BY</p>
@@ -45,16 +48,20 @@ const Hero = () => {
                     </div>
                 </div>
 
-                <div className="flex items-center pb-3 gap-4 ml-[5%] lg:ml-[7%] mt-6">
-                    <Button word="Apply Now" className="bg-white rounded p-2 h-[48px] lg:h-[132px] text-black px-4 lg:px-6 py-2 text-sm" />
-                    <p className="mt-2 text-[14px] lg:text-[16px] w-[200px] lg:w-[265px] h-[21px]">
+                <div className="flex items-center pb-3 gap-4 ml-[5%] lg:ml-[7%] mt-8">
+
+                    <Link href={''}>
+                        <Button word="Apply Now" className="bg-white rounded p-2 h-[48px] lg:h-[132px] text-black px-4 lg:px-6 py-2 text-sm" />
+                    </Link>
+
+                    <p className="mt-2 text-[14px] lg:text-[16px]  lg:w-[265px] h-[21px]">
                         5,000 students already enrolled
                     </p>
                 </div>
             </div>
 
-            <div className="w-full mt-4 lg:w-[40%] lg:mt-0">
-                <Image src={heroImage} alt="heroImage" className="w-full h-auto object-cover" />
+            <div className=' w-[40%] lg:mt-0'>
+                <Image src={heroImage} alt="heroImage" className=' h-[1500px]' />
             </div>
         </div>
     );
--- component/partners.tsx
+++ component/partners.tsx
@@ -5,7 +5,7 @@ import {Icon} from '@iconify/react';
 const Partners=()=>{
     return(
         <div className={'mt-8  mb-4 '}>
-            <div className={'flex gap-2 ml-[5%] mr-[5%]' } style={{fontSize:'10px'}}>
+            <div className={'flex gap-2 ml-[5%] mr-[5%]' } >
                 <p className={'text-black m-3  mr-0 ml-0'}>Partners:</p>
                 <Icon icon="fluent:chevron-left-24-filled" className={'text-black h-5 w-5 mt-3 '}/>
                 <div className="border-l-2 border-gray-300 h-10 mt-1"></div>
