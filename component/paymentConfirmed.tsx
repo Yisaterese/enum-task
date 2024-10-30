88dabde929e8de8a6c47189934d3357cf134029b
Yisaterese <teresejosephyisa@gmail.com>
2024-10-30 12:05:58 +0100

    restyled the button component using traditional css because tailwind css is failing on the server

--- component/paymentConfirmed.tsx
+++ component/paymentConfirmed.tsx
@@ -6,11 +6,11 @@ import {ConfirmPaymentProps} from "../types/types";
 import Link from 'next/link';
 const ConfirmPayment:React.FC<ConfirmPaymentProps>=({text,link})=>{
     return(
-        <div className={'px-[400px] justify-items-center'}>
-            <div className={'  my-[100px] mb-8 mt-[70px]'}>
+        <div className={''} style={{paddingRight:'400px',paddingLeft:'400px',justifyItems:'center'}}>
+            <div className={" "} style={{margin:'70px 32px'}}>
                 <Image src={SuccessIcon} alt={'successIcon'} className={' w-[100px] h-[100px] '} />
             </div>
-            <div className={'text-black justify-items-center   '}>
+            <div className={'text-black justify-items-center '}>
                 <p className={'text-lg font-bold py-5  mx-[120px] justify-items-center'}>Payment Confirmed, check <p >your Email</p></p>
                 <p className={'text-[12px] py-5 justify-items-center '}>Your payment has been confirmed, please check your Email <p >
                     for the
@@ -18,7 +18,7 @@ const ConfirmPayment:React.FC<ConfirmPaymentProps>=({text,link})=>{
                 </p>
                 </p>
                 <Link href={link}>
-                    <Button word={text} className='bg-blue-600 rounded text-white px-2 py-2  text-[13px]'/>
+                    <Button word={text} className="butto" />
                 </Link>
 
             </div>
