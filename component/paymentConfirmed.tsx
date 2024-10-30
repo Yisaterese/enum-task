f67b43bdaae6f011a81c32f665bbe0df360b4e90
Yisaterese <teresejosephyisa@gmail.com>
2024-10-30 17:29:27 +0100

    restyled applicationFeepayment

--- component/paymentConfirmed.tsx
+++ component/paymentConfirmed.tsx
@@ -7,19 +7,27 @@ import Link from 'next/link';
 const ConfirmPayment:React.FC<ConfirmPaymentProps>=({text,link})=>{
     return(
         <div className={''} style={{paddingRight:'400px',paddingLeft:'400px',justifyItems:'center'}}>
-            <div className={" "} style={{margin:'70px 32px'}}>
+            <div  style={{margin:'70px 32px 40px '}}>
                 <Image src={SuccessIcon} alt={'successIcon'} className={' w-[100px] h-[100px] '} />
             </div>
+
             <div className={'text-black justify-items-center '}>
-                <p className={'text-lg font-bold py-5  mx-[120px] justify-items-center'}>Payment Confirmed, check <p >your Email</p></p>
-                <p className={'text-[12px] py-5 justify-items-center '}>Your payment has been confirmed, please check your Email <p >
+                <p className={'text-lg font-bold  '} style={{margin:'0 120px 0 120px',justifyItems:'center'}} >Payment Confirmed, check <p >your Email</p></p>
+                <p className={''} style={{
+                    fontSize:'12px',
+                    padding:'20px 0 20px 0',
+                    justifyItems:'center'
+                }}>Your payment has been confirmed, please check your Email <p >
                     for the
                     Assessment link or click the button below.
                 </p>
                 </p>
-                <Link href={link}>
-                    <Button word={text} className="butto" />
-                </Link>
+                <div className={' '}>
+                    <Link href={link}>
+                        <Button word={text} className="button" />
+                    </Link>
+                </div>
+
 
             </div>
         </div>
