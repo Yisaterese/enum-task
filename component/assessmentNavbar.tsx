6d5ded0ddbf81274b5f69617d299a177df242ebf
Yisaterese <teresejosephyisa@gmail.com>
2024-11-01 09:03:40 +0100

    added code

--- component/assessmentNavbar.tsx
+++ component/assessmentNavbar.tsx
@@ -11,7 +11,7 @@ const AssessmentNavbar:React.FC<AssessmentNavbarProps> =({link})=>{
             setStyling(!styling)
     }
     return (
-        <div className='p-0.5 border-2 py-4 ] '>
+        <div className='p-0.5 border-2 py-4 ' style={{padding:'24px'}}>
             <div className='flex justify-between mt-1 ml-[4%] mr-[4%]  '>
                 <Image src={enumImage} alt='Enum Image' className=''/>
                 <Link href={link} className={`p-[10px_5px] ${!styling?'text-blue-500 rounded border border-blue-500 py-2 px-6': `${styling}`}`} onClick={changeColor}>cancel</Link>
